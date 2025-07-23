30281d7f0c9e1d3078677523e53bf2f531c85136
yarasa27 <elcan.abbasoff27@gmail.com>
2025-07-24 00:41:27 +0400

    blurheader JS and CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,12 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.add('blur-header')                   
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== GSAP ANIMATION ===============*/
 

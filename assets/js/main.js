e9295e687678d992458090ab8787cf69948e4a78
yarasa27 <elcan.abbasoff27@gmail.com>
2025-07-24 00:49:06 +0400

    gsap animation in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const blurHeader = () => {
     const header = document.getElementById('header')
     this.scrollY >= 50 ? header.classList.add('blur-header')
-                       : header.classList.add('blur-header')                   
+                       : header.classList.remove('blur-header')                   
 }
 window.addEventListener('scroll', blurHeader)
 
 /*=============== GSAP ANIMATION ===============*/
-
+gsap.from('.home__img-1', 5, {opacity: 0, x: 400})
+gsap.from('.home__img-6', 1.5, {opacity: 0, y: 200, delay: .1, ease: 'back.out(1.5)'})
+gsap.from('.home__img-3', 1.5, {opacity: 0, y: 400, delay: .3, ease: 'back.out(1.5)'})
+gsap.from('.home__img-5', 1.5, {opacity: 0, y: 400, delay: .5, ease: 'back.out(1.5)'})
+gsap.from('.home__img-2', 1.5, {opacity: 0, y: 400, delay: .8, ease: 'back.out(1.5)'})
+gsap.from('.home__img-4', 1.5, {opacity: 0, y: 200, delay: 1.8, ease: 'back.out(1.5)'})
+gsap.from('.home__data', 1.5, {opacity: 0, y: -100, delay: 2.5, ease: 'back.out(1.5)'})
+gsap.from('.home__lantern-1', 1.5, {opacity: 0, x: 70, delay: 3, ease: 'back.out(1.5)'})
+gsap.from('.home__lantern-2', 1.5, {opacity: 0, x: -70, delay: 3.5, ease: 'back.out(1.5)'})
 
 /*=============== SAKURA ANIMATION ===============*/
 
